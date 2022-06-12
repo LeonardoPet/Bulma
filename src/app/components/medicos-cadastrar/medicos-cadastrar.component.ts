d6bf8938a85df0f778fb86ae1f01cf0b826c7bfe
LeonardoELuz <leo.e.luz@gmail.com>
2022-06-12 01:17:19 -0300

    update

--- src/app/components/medicos-cadastrar/medicos-cadastrar.component.ts
+++ src/app/components/medicos-cadastrar/medicos-cadastrar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-medicos-cadastrar',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./medicos-cadastrar.component.css']
 })
 export class MedicosCadastrarComponent implements OnInit {
-
-  constructor() { }
+  formCadastroMedico!: FormGroup;
+  especialidade: string
+  nome: string
+  constructor(private web:Webservice) { }
 
   ngOnInit(): void {
+    this.formInit
   }
 
+  formInit(){
+    this.formCadastroMedico = new FormGroup({
+      nome : new FormControl(null, [Validators.required]), especialidade : new FormControl(null, [Validators.required])
+    });
+  }
+
+  realizarCadastroMedico(){
+    if(this.formCadastroMedico.valid){
+      let nome = this.formCadastroMedico.get("nome")!.value
+      let especialidade = this.formCadastroMedico.get("especialidade")!.value
+      this.web.realizarCadastroMedico(nome,especialidade).subscribe(res => {
+        if(res.status == "OK") 
+        {
+          alert("Você foi cadastrado com sucesso")
+        } 
+        else
+        {
+          alert("O Cadastro falhou")
+        }
+      });
+    }  
+  }
 }
--- src/app/components/pacientes-cadastro/pacientes-cadastro.component.ts
+++ src/app/components/pacientes-cadastro/pacientes-cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-pacientes-cadastro',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pacientes-cadastro.component.css']
 })
 export class PacientesCadastroComponent implements OnInit {
+  formCadastroPaciente!: FormGroup;
+  dataNasc: string
+  nome: string
 
-  constructor() { }
+  constructor(private web:Webservice) { }
 
   ngOnInit(): void {
+    this.formInit
+  }
+  formInit(){
+    this.formCadastroPaciente = new FormGroup({
+      nome : new FormControl(null, [Validators.required]), dataNasc : new FormControl(null, [Validators.required])
+    });
   }
 
+
+
+  realizarCadastroPaciente(){
+    if(this.formCadastroPaciente.valid){
+      let nome = this.formCadastroPaciente.get("nome")!.value
+      let dataNasc = this.formCadastroPaciente.get("dataNasc")!.value
+      this.web.realizarCadastroPaciente(nome,dataNasc).subscribe(res => {
+        if(res.status == "OK") 
+        {
+          alert("Você foi cadastrado com sucesso")
+        } 
+        else
+        {
+          alert("O Cadastro falhou")
+        }
+      });
+    }  
+  }
 }
